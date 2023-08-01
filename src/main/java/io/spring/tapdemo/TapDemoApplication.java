8e578eb768c67c61580b11898a150b15e196e966
Robert McNees <rmcnees@vmware.com>
2023-08-01 08:26:01 -0400

    Initial commit

---
+++ src/main/java/io/spring/tapdemo/TapDemoApplication.java
@@ -0,0 +1,13 @@
+package io.spring.tapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/tapdemo/TapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.tapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
